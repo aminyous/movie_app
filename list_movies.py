eef8fe196bf506dfede51464df52e4edccbbc05c
aminyous <amyn.hichri@gmail.com>
2023-12-25 17:58:08 +0100

    my second commit just to test things out

---
+++ list_movies.py
@@ -0,0 +1,2 @@
+def list_movies(movies):
+    return [print(f"movie {movie}, director, year") for movie in movies]
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from list_movies import list_movies
+
 print("*** Welcome to the Movie App ***")
+
+movies = ["Movie_1", "Movie_2", "Movie_3"]
+
+selection = input("Please enter 'A' for add, 'L' for show, 'Q' for quit").lower()
+
+while selection != 'q':
+    if selection == "a":
+        pass
+    elif selection == "l":
+        list_movies(movies)
+    elif selection == "q":
+        break
+    else:
+        print("Your choice is not valid.")
+
+    selection = input("Please enter 'A' for add, 'L' for show, 'Q' for quit").lower()
+
+print("*** GOOD BYE ***")
